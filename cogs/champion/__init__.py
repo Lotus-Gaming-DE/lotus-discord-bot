997c869b3fbe2af49110fc998b230427d0ee0c2c
gs3rr4 <gs3rr4@gmail.com>
2025-06-02 00:50:10 +0200

    Update __init__.py

--- cogs/champion/__init__.py
+++ cogs/champion/__init__.py
@@ -1,9 +1,7 @@
 import os
 import discord
 import logging
-
 from .cog import ChampionCog
-from .slash_commands import champion_group
 
 logger = logging.getLogger(__name__)  # cogs.champion.__init__
 
@@ -16,11 +14,6 @@ MAIN_SERVER_ID = int(SERVER_ID)
 async def setup(bot: discord.ext.commands.Bot):
     try:
         await bot.add_cog(ChampionCog(bot))
-        bot.tree.add_command(
-            champion_group,
-            guild=discord.Object(id=MAIN_SERVER_ID)
-        )
-        logger.info(
-            "[ChampionCog] Cog und Slash-Command-Gruppe erfolgreich registriert.")
+        logger.info("[ChampionCog] Cog erfolgreich geladen.")
     except Exception as e:
         logger.error(f"[ChampionCog] Fehler beim Setup: {e}", exc_info=True)
