0cfa21b45f484a39a9a7b61000d1df8688d874cf
gs3rr4 <gs3rr4@gmail.com>
2025-06-06 01:28:15 +0200

    Remove redundant quiz generator init

--- cogs/quiz/__init__.py
+++ cogs/quiz/__init__.py
@@ -4,9 +4,7 @@ from log_setup import get_logger
 
 from .cog import QuizCog
 from .slash_commands import quiz_group
-from .question_state import QuestionStateManager
-from .question_generator import QuestionGenerator
-from .utils import get_available_areas
+
 
 
 logger = get_logger(__name__)
@@ -15,29 +13,6 @@ logger = get_logger(__name__)
 async def setup(bot: commands.Bot):
     """Set up the quiz cog and slash commands."""
     logger.info("[QuizInit] Initialisierung startet...")
-
-    from bot import QUESTION_STATE_PATH
-
-    areas = get_available_areas()
-    state_manager = QuestionStateManager(QUESTION_STATE_PATH)
-    dynamic_providers = {}
-
-    for area in areas:
-        try:
-            module = __import__(
-                f"cogs.quiz.area_providers.{area}", fromlist=["get_provider"])
-            dynamic_providers[area] = module.get_provider(bot, language="de")
-        except ModuleNotFoundError as e:
-            logger.warning(
-                f"[QuizInit] Kein dynamischer Provider für '{area}': {e}")
-
-    generator = QuestionGenerator(
-        questions_by_area=bot.quiz_data.get("questions", {}),
-        state_manager=state_manager,
-        dynamic_providers=dynamic_providers,
-    )
-
-    bot.quiz_generator = generator  # expose for potential use
 
     quiz_cog = QuizCog(bot)
     await bot.add_cog(quiz_cog, override=True)
