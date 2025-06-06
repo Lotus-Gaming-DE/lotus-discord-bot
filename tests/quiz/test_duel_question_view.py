cb1987f8059af8379097b63b7a03657819f83fe6
gs3rr4 <gs3rr4@gmail.com>
2025-06-06 14:13:17 +0200

    Ignore duel answers after view finished

--- tests/quiz/test_duel_question_view.py
+++ tests/quiz/test_duel_question_view.py
@@ -2,7 +2,7 @@ import datetime
 import pytest
 
 
-from cogs.quiz.duel import DuelQuestionView
+from cogs.quiz.duel import DuelQuestionView, _DuelAnswerModal
 
 
 class DummyMember:
@@ -10,6 +10,21 @@ class DummyMember:
         self.id = uid
         self.display_name = f"user{uid}"
         self.mention = f"<@{uid}>"
+
+
+class DummyResponse:
+    def __init__(self):
+        self.sent = []
+
+    async def send_message(self, content, **kwargs):
+        self.sent.append((content, kwargs))
+
+
+class DummyInteraction:
+    def __init__(self, user):
+        self.user = user
+        self.response = DummyResponse()
+        self.created_at = datetime.datetime.utcnow()
 
 
 @pytest.mark.asyncio
@@ -28,3 +43,22 @@ async def test_finish_sets_winner_and_disables_buttons():
 
     assert view.winner_id == opponent.id
     assert all(child.disabled for child in view.children)
+
+
+@pytest.mark.asyncio
+async def test_modal_ignores_after_finish():
+    challenger = DummyMember(1)
+    opponent = DummyMember(2)
+    view = DuelQuestionView(challenger, opponent, ["yes"])
+
+    await view._finish()
+    modal = _DuelAnswerModal(view)
+    modal.answer._value = "foo"
+    inter = DummyInteraction(challenger)
+
+    await modal.on_submit(inter)
+
+    assert view.responses == {}
+    assert inter.response.sent == [
+        ("Die Runde ist bereits beendet.", {"ephemeral": True})
+    ]
